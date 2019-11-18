414216d6a32a9f681043f04332782dfa8370639f
Lukho Mdingi <lukho@bbd.co.za>
2019-11-18 02:05:23 +0200

    Created a new game loop.

--- game.js
+++ game.js
@@ -1,14 +1,32 @@
-
-class Game {
-    constructor(display) {
-        this.display = display;
+// Constant values
+const FPS = 30;
+class GameController {
+    elapsed = 0;
+    lastRender = 0;
+    constructor() {
+        this.hasStarted = false;
     }
 
-    updateAction(action) {
-        this.display.playerAction = action;
+    start = () => {
+        this.hasStarted = true;
+        window.requestAnimationFrame(this.loop);
     }
 
-    update() {        
-        this.display.render();
+    loop = (timestamp) => {
+        const elapsed = timestamp - this.lastRender;
+        const fpsInterval = 1000 / FPS;
+        if (elapsed > fpsInterval) {
+            this.update();
+        }
+
+        this.lastRender = timestamp - (elapsed % fpsInterval);
+        window.requestAnimationFrame(this.loop);
+    }
+    
+    update = () => {
+        if (!this.hasStarted)
+            throw 'The game has not been started.'
+        displayController.render();
+
     }
 }
