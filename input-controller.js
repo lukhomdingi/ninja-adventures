4359d0d2ed59df67f24ee35d3d572f7195ea2d13
Lukho Mdingi <lukho@bbd.co.za>
2019-11-30 21:40:42 +0200

    Switched controller keys.

--- input-controller.js
+++ input-controller.js
@@ -8,17 +8,17 @@ class InputController {
         event.preventDefault();
 
         switch(event.key.toLowerCase()) {
-            case ' ': actionController.attack(); break;
+            case 'k': actionController.attack(); break;
             case 'c': actionController.climb(); break;
             case 'g': actionController.glide(); break;
             case 'w':
             case 'arrowup': actionController.jump(); break;
             case 'a':
-            case 'arrowleft': actionController.run('Left'); break; //use Direction.Left
+            case 'arrowleft': actionController.run(DIRECTION.LEFT); break;
             case 'd':
-            case 'arrowright': actionController.run('Right'); break; //use Direction.Right
-            case 'l': actionController.slide(); break;
-            case 'k': actionController.throw(); break;
+            case 'arrowright': actionController.run(DIRECTION.RIGHT); break;
+            case 's': actionController.slide(); break;
+            case 'l': actionController.throw(); break;
         }
     }
 
